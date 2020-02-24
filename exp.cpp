fbec0ea2595b8dc3da18c708593698b34e972e17
Lorsman <mailbox556556@gmail.com>
2020-02-25 07:12:22 +0800

    change the origin of the asteroid

--- exp.cpp
+++ exp.cpp
@@ -16,11 +16,23 @@ int main()
 {
     std::vector<sSpaceObject> vecAsteroids{};
     vecAsteroids.push_back({50.f, 50.f, 100.f, 100.f, 0.0f, 1.0f});
-    sf::CircleShape asteroidModelShape(100.0f);
+    float fRadius = 100.0f;
+    sf::CircleShape asteroidModelShape(fRadius);
     asteroidModelShape.setFillColor(sf::Color(0, 0, 0));
     asteroidModelShape.setOutlineColor(sf::Color(250, 150, 100));
     asteroidModelShape.setOutlineThickness(1.0f);
-    
+    asteroidModelShape.setOrigin(fRadius/2, fRadius/2);
+
+    sSpaceObject ship{float(nScreenWidth)/2.0f, float(nScreenHeight)/2.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    //sf::CircleShape shipModelShape(20.0f, 3);
+    sf::ConvexShape shipModelShape;
+    float ship_box_h = 24.0f, ship_box_w = 15.0f;
+    shipModelShape.setOrigin(ship_box_w / 2.0f, ship_box_h / 2.0f);
+    shipModelShape.setPointCount(3);
+    shipModelShape.setPoint(0, sf::Vector2f(0.0, -ship_box_h*2.0f/3.0f));
+    shipModelShape.setPoint(1, sf::Vector2f(-ship_box_w/2.0f, ship_box_h/3.0f));
+    shipModelShape.setPoint(2, sf::Vector2f(ship_box_w/2.0f, ship_box_h/3.0f));
+    shipModelShape.setFillColor(sf::Color(255, 255, 255));
 
     sf::RenderWindow window(sf::VideoMode(nScreenWidth, nScreenHeight), "");
 
@@ -42,6 +54,11 @@ int main()
 
         // clear the window
         window.clear(sf::Color::Black);
+
+        // draw and update player ship
+        shipModelShape.setPosition(ship.x, ship.y);
+        shipModelShape.rotate(ship.angle);
+        window.draw(shipModelShape);
 
         // draw and update asteroids
         for(auto &a : vecAsteroids)
