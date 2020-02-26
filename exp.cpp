970e40cdc6a724eb0eb34232c70bc66dbd94725a
Lorsman <mailbox556556@gmail.com>
2020-02-26 09:50:58 +0800

    game reset and bDead

--- exp.cpp
+++ exp.cpp
@@ -1,11 +1,11 @@
-#include<SFML/Graphics.hpp>
-#include<SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <vector>
 #include <algorithm>
 constexpr auto PI = 3.14159265f;
 
-int nScreenWidth = 1000;
-int nScreenHeight = 800;
+int nScreenWidth = 800;
+int nScreenHeight = 600;
 
 void wrapCoordinate(float &x, float &y);
 bool isPointInsideCircle(float cx, float cy, float r, float x, float y);
@@ -24,29 +24,29 @@ int main()
     // =======ASTEROID SETUP=======
     std::vector<sSpaceObject> vecAsteroids{};
     vecAsteroids.push_back({50.f, 50.f, 100.f, 100.f, 0.0f, 2.0f});
-    float fAsteroidRad = 50.0f;
+    float fAsteroidRad = 70.f;
     sf::CircleShape asteroidModelShape(fAsteroidRad);
     asteroidModelShape.setFillColor(sf::Color::Transparent);
     asteroidModelShape.setOutlineColor(sf::Color(250, 150, 100));
     asteroidModelShape.setOutlineThickness(1.0f);
-    asteroidModelShape.setOrigin(fAsteroidRad/2, fAsteroidRad/2);
+    asteroidModelShape.setOrigin(fAsteroidRad, fAsteroidRad);
 
     // =======SHIP SETUP=======
-    sSpaceObject ship{float(nScreenWidth)/2.0f, float(nScreenHeight)/2.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    sSpaceObject ship{float(nScreenWidth) / 2.0f, float(nScreenHeight) / 2.0f, 0.0f, 0.0f, 0.0f, 0.0f};
     sf::ConvexShape shipModelShape;
-    float ship_box_h = 24.0f, ship_box_w = 15.0f;
+    float ship_box_h = 48.0f, ship_box_w = 30.0f;
     shipModelShape.setPointCount(3);
-    shipModelShape.setPoint(0, sf::Vector2f(0.0, -ship_box_h*2.0f/3.0f));
-    shipModelShape.setPoint(1, sf::Vector2f(-ship_box_w/2.0f, ship_box_h/3.0f));
-    shipModelShape.setPoint(2, sf::Vector2f(ship_box_w/2.0f, ship_box_h/3.0f));
+    shipModelShape.setPoint(0, sf::Vector2f(0.0, -ship_box_h * 2.0f / 3.0f));
+    shipModelShape.setPoint(1, sf::Vector2f(-ship_box_w / 2.0f, ship_box_h / 3.0f));
+    shipModelShape.setPoint(2, sf::Vector2f(ship_box_w / 2.0f, ship_box_h / 3.0f));
     shipModelShape.setFillColor(sf::Color(255, 255, 255));
-    float shipAngVel = 200.0f;  // Degree per second
-    float shipAcc = 150.0f;     // px per second
+    float shipAngVel = 200.0f; // Degree per second
+    float shipAcc = 150.0f;    // px per second
 
     // ======BULLET SETUP======
     std::vector<sSpaceObject> vecBullets{};
-    sf::RectangleShape bulletModelShape(sf::Vector2f(3.0f, 3.0f));
-    float bulletVec = 500.f;    // px per sec
+    sf::RectangleShape bulletModelShape(sf::Vector2f(3.0f, 5.0f));
+    float bulletVec = 500.f; // px per sec
     bulletModelShape.setFillColor(sf::Color::White);
 
     // ======CREATE WINDOW======
@@ -55,6 +55,17 @@ int main()
 
     sf::Clock triggerClock;
     sf::Clock clock;
+    bool bDead = false;
+    auto ResetGame = [&]() {
+        vecAsteroids.clear();
+        vecBullets.clear();
+        vecAsteroids.push_back({50.f, 50.f, 100.f, 60.f, 0.0f, 2.0f});
+        vecAsteroids.push_back({600.f, 100.f, -50.f, 20.f, 0.0f, 2.8f});
+        vecAsteroids.push_back({50.f, 0.f, 150.f, -10.f, 0.0f, 1.5f});
+        vecAsteroids.push_back({600., 500.f, 5.f, -30.f, 0.0f, 2.5f});
+        ship = {float(nScreenWidth) / 2.0f, float(nScreenHeight) / 2.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+        bDead = false;
+    };
     // run the program as long as the window is open
     while (window.isOpen())
     {
@@ -70,12 +81,15 @@ int main()
                 window.close();
         }
 
+        if (bDead == true)
+            ResetGame();
+
         // steer the ship
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
             ship.angle += shipAngVel * fElapsedTime;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))   
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
             ship.angle -= shipAngVel * fElapsedTime;
-        
+
         // thrust the ship
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         {
@@ -89,24 +103,23 @@ int main()
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && triggerDelay > 0.25f)
         {
             triggerClock.restart();
-            float bulletTriggerPos_x = 
-                ship.x + 
-                shipModelShape.getPoint(0).x * cosf(ship.angle * PI / 180) - 
+            float bulletTriggerPos_x =
+                ship.x +
+                shipModelShape.getPoint(0).x * cosf(ship.angle * PI / 180) -
                 shipModelShape.getPoint(0).y * sinf(ship.angle * PI / 180);
 
-            float bulletTriggerPos_y =  
-                ship.y + 
-                shipModelShape.getPoint(0).y * cosf(ship.angle * PI / 180) + 
+            float bulletTriggerPos_y =
+                ship.y +
+                shipModelShape.getPoint(0).y * cosf(ship.angle * PI / 180) +
                 shipModelShape.getPoint(0).x * sinf(ship.angle * PI / 180);
 
             sSpaceObject bullet{
                 bulletTriggerPos_x,
-                bulletTriggerPos_y, 
+                bulletTriggerPos_y,
                 ship.vx + bulletVec * sinf(ship.angle * PI / 180.f), // add initial vel
-                ship.vy + bulletVec * -cosf(ship.angle * PI / 180.f), 
-                ship.angle, 
-                1.0f
-            };
+                ship.vy + bulletVec * -cosf(ship.angle * PI / 180.f),
+                ship.angle,
+                1.0f};
             vecBullets.push_back(bullet);
         }
 
@@ -123,27 +136,26 @@ int main()
 
         // draw and update bullets and check bullet collision
         std::vector<sSpaceObject> vecNewAsteroids{};
-        for(auto &b : vecBullets)
+        for (auto &b : vecBullets)
         {
             b.x += b.vx * fElapsedTime;
             b.y += b.vy * fElapsedTime;
             bulletModelShape.setPosition(b.x, b.y);
             window.draw(bulletModelShape);
-            
+
             // Check for asteroid collision
-            for(auto &a : vecAsteroids)
+            for (auto &a : vecAsteroids)
             {
-                if(isPointInsideCircle(a.x, a.y, a.size * fAsteroidRad, b.x, b.y))
+                if (isPointInsideCircle(a.x, a.y, a.size * fAsteroidRad, b.x, b.y))
                 {
                     b.x -= 1000.0f;
 
                     // split the asteroid if size > 4 else delete it
-                    if(a.size > 0.5f)
+                    if (a.size > 0.5f)
                     {
                         float angle1 = float(rand()) / float(RAND_MAX) * 360.f; //Degree not radian
                         float angle2 = float(rand()) / float(RAND_MAX) * 360.f;
                         float splitVec = 50.f;
-                        printf("hit hit hit!\n");
                         vecNewAsteroids.push_back({a.x, a.y, splitVec * sind(angle1), splitVec * -cosd(angle1), angle1, a.size / 2.0f});
                         vecNewAsteroids.push_back({a.x, a.y, splitVec * sind(angle2), splitVec * -cosd(angle2), angle2, a.size / 2.0f});
                     }
@@ -153,45 +165,53 @@ int main()
         }
 
         // append new asteroid in original storage
-        for(auto a : vecNewAsteroids)
+        for (auto a : vecNewAsteroids)
             vecAsteroids.push_back(a);
 
         // remove bullet if it is off the screen
-        if(!vecBullets.empty())
+        if (!vecBullets.empty())
         {
             auto iter = std::remove_if(
                 vecBullets.begin(),
                 vecBullets.end(),
-                [&](sSpaceObject o) { return (o.x < 1.0f || o.y < 1.0f || o.x >= nScreenWidth-1 || o.y >= nScreenHeight-1);}
-            );
-            if(iter != vecBullets.end())
+                [&](sSpaceObject o) { return (o.x < 1.0f || o.y < 1.0f || o.x >= nScreenWidth - 1 || o.y >= nScreenHeight - 1); });
+            if (iter != vecBullets.end())
                 vecBullets.erase(iter);
         }
-        
+
         // remove asteroid if it is off the screen
-        if(!vecAsteroids.empty())
+        if (!vecAsteroids.empty())
         {
             auto iter = std::remove_if(
                 vecAsteroids.begin(),
                 vecAsteroids.end(),
-                [&](sSpaceObject o) { return (o.x < 0.0f);}
-            );
-            if(iter != vecAsteroids.end())
+                [&](sSpaceObject o) { return (o.x < 0.0f); });
+            if (iter != vecAsteroids.end())
                 vecAsteroids.erase(iter);
         }
 
         // draw and update asteroids
-        for(auto &a : vecAsteroids)
+        for (auto &a : vecAsteroids)
         {
             a.x += a.vx * fElapsedTime;
             a.y += a.vy * fElapsedTime;
             wrapCoordinate(a.x, a.y);
-            
+
             asteroidModelShape.setPosition(a.x, a.y);
             asteroidModelShape.setScale(a.size, a.size);
             //asteroidModelShape.rotate(a.angle);
             window.draw(asteroidModelShape);
+
+            //debug
+            sf::RectangleShape originRec(sf::Vector2f(3.f, 3.f));
+            originRec.setFillColor(sf::Color::Red);
+            originRec.setPosition(a.x, a.y);
+            window.draw(originRec);
         }
+
+        for (auto a : vecAsteroids)
+            if (isPointInsideCircle(a.x, a.y, fAsteroidRad * a.size, ship.x, ship.y))
+                bDead = true;
 
         window.display();
     }
@@ -199,18 +219,21 @@ int main()
     return 0;
 }
 
-
 //----------------FUCTION DECLARATIONS------------------
 void wrapCoordinate(float &x, float &y)
 {
-    if(x <= 0.0f)           x += nScreenWidth;
-    if(x >= nScreenWidth)   x -= nScreenWidth;
-    if(y <= 0.0f)           y += nScreenHeight;
-    if(y >= nScreenHeight)  y -= nScreenHeight;
+    if (x <= 0.0f)
+        x += nScreenWidth;
+    if (x >= nScreenWidth)
+        x -= nScreenWidth;
+    if (y <= 0.0f)
+        y += nScreenHeight;
+    if (y >= nScreenHeight)
+        y -= nScreenHeight;
 }
 
 bool isPointInsideCircle(float cx, float cy, float r, float x, float y)
 {
-    float dis = sqrt((x - cx)*(x - cx) + (y - cy)*(y - cy));
+    float dis = sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
     return (dis < r) ? true : false;
 }
